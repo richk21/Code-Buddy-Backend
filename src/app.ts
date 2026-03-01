69ff3814cbe41f906a3988a2d1039a4b4aefed1a
richa <richa21kiran@gmail.com>
2026-03-01 17:45:29 +0530

    CORS added

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,21 @@ import authRoutes from "./routes/AuthRoutes";
 import tasksRoutes from "./routes/TaskRoutes";
 
 const app = express();
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
